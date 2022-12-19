4dcd00517f0782275c030b13ec05fb94f5bcd19b
Christopher González Wienberg <christopher@gonzberg.net>
2022-12-18 17:03:05 -0800

    Add classes for using an async map over an iterator

---
+++ src/main/scala/net/gonzberg/spark/async/util/BatchAsyncMapIterator.scala
@@ -0,0 +1,54 @@
+package net.gonzberg.spark.async.util
+
+import java.util.concurrent.{ArrayBlockingQueue, BlockingQueue, ExecutorService, ThreadPoolExecutor, TimeUnit}
+import scala.collection.mutable
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
+
+private[async] object BatchAsyncMapIterator {
+
+  val DEFAULT_BUFFER_SIZE: Int = 1
+  val KEEP_ALIVE_TIME: Long = 15
+  val KEEP_ALIVE_TIME_UNIT: TimeUnit = TimeUnit.SECONDS
+
+  private def createThreadPoolExecutionContext(corePoolSize: Int, maxPoolSize: Int, bufferSize: Int): ExecutionContext = {
+    val workQueue: BlockingQueue[Runnable] = new ArrayBlockingQueue(bufferSize)
+    val threadPoolExecutor: ExecutorService = new ThreadPoolExecutor(
+      corePoolSize, maxPoolSize, KEEP_ALIVE_TIME, KEEP_ALIVE_TIME_UNIT, workQueue
+    )
+    ExecutionContext.fromExecutorService(threadPoolExecutor)
+  }
+
+  def apply[A,B](input: Iterator[A], op: A => B, bufferSize: Int = DEFAULT_BUFFER_SIZE): Iterator[B] = {
+    implicit val executionContext = createThreadPoolExecutionContext(Math.max(1, bufferSize / 2), bufferSize, bufferSize)
+    BatchAsyncMapIterator.delegate(input, (a: A) => Future{op(a)}, bufferSize)
+  }
+
+  def delegate[A,B](input: Iterator[A], op: A => Future[B], bufferSize: Int = DEFAULT_BUFFER_SIZE): Iterator[B] = {
+    new BatchAsyncMapIterator[A, B](input, op, bufferSize)
+  }
+}
+
+private[async] class BatchAsyncMapIterator[A, B](input: Iterator[A], op: A => Future[B], bufferSize: Int = BatchAsyncMapIterator.DEFAULT_BUFFER_SIZE) extends Iterator[B] {
+
+  private[this] val buffer: mutable.Queue[Future[B]] = new mutable.Queue(bufferSize)
+  while (input.hasNext && (buffer.size < bufferSize - 1)) {
+    shiftToBuffer()
+  }
+
+  private[this] def shiftToBuffer(): Unit = {
+    if (input.hasNext) {
+      buffer.enqueue(op(input.next()))
+    }
+  }
+
+  override def hasNext: Boolean = {
+    buffer.nonEmpty || input.hasNext
+  }
+
+  override def next(): B = {
+    require(hasNext, "Called next on empty iterator")
+    shiftToBuffer()
+    Await.result(buffer.dequeue(), Duration.Inf)
+  }
+}
---
+++ src/test/scala/net/gonzberg/spark/async/util/BatchAsyncMapIteratorTest.scala
@@ -0,0 +1,37 @@
+package net.gonzberg.spark.async.util
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+import java.util.concurrent.Executors
+import scala.concurrent.{ExecutionContext, Future}
+
+class BatchAsyncMapIteratorTest extends AnyFunSuite with Matchers {
+
+  val BUFFER_SIZE: Int = 5
+  val FULL_INPUT: Vector[Int] = 1.to(100).toVector
+  val EXPECTED_FULL_OUTPUT: Array[Int] = FULL_INPUT.map(_*3).toArray
+
+  implicit val testExecutionContext: ExecutionContext = ExecutionContext.fromExecutorService(Executors.newSingleThreadExecutor())
+
+  test("testApplyEmpty") {
+    val actualOutput = BatchAsyncMapIterator.apply(Iterator.empty[Int], (x: Int) => x * 3, BUFFER_SIZE).toVector
+    assert(actualOutput.isEmpty)
+  }
+
+  test("testApplyFull") {
+    val actualOutput = BatchAsyncMapIterator.apply(FULL_INPUT.iterator, (x: Int) => x * 3, BUFFER_SIZE).toVector
+    EXPECTED_FULL_OUTPUT should contain theSameElementsInOrderAs actualOutput
+  }
+
+  test("testDelegateEmpty") {
+    val actualOutput = BatchAsyncMapIterator.delegate(Iterator.empty[Int], (x: Int) => Future{x * 3}, BUFFER_SIZE).toVector
+    assert(actualOutput.isEmpty)
+  }
+
+  test("testDelegateFull") {
+    val actualOutput = BatchAsyncMapIterator.delegate(FULL_INPUT.iterator, (x: Int) => Future{x * 3}, BUFFER_SIZE).toVector
+    EXPECTED_FULL_OUTPUT should contain theSameElementsInOrderAs actualOutput
+  }
+
+}
